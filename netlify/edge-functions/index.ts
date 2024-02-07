7150f2a83a8627344f9b940217aefe13569b690f
Yoshio HANAWA <y@hnw.jp>
2024-02-08 00:05:06 +0900

    Hono経由でproxyするようにした / netlify.tomlだけで動くように修正

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -1,10 +1,12 @@
-import { handle } from 'https://deno.land/x/hono@v3.12.8/adapter/netlify/mod.ts'
 import { Hono } from 'https://deno.land/x/hono@v3.12.8/mod.ts'
+import { handle } from 'https://deno.land/x/hono@v3.12.8/adapter/netlify/mod.ts'
+import type { Env } from 'https://deno.land/x/hono@v3.12.8/adapter/netlify/mod.ts'
 
-const app = new Hono()
+const app = new Hono<Env>()
 
-app.get('/country', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/country', (c) =>
+  c.text(`You are in ${c.env.context.geo.country?.name}`)
+)
+app.get('*', async (c) => c.env.context.next())
 
 export default handle(app)
